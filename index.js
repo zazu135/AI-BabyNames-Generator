79fbbc158d4965fa29c574373687fb7f44187f4a
zazu135 <142782991+zazu135@users.noreply.github.com>
2024-08-04 13:45:21 +0400

    Finalized styling and functions

--- index.js
+++ index.js
@@ -17,13 +17,19 @@ function generatebabynames(gender) {
     let context = "You are a baby name generator and you love to generate 10 unique names in basic HTML in numbered list. Please follow the user instructions and generate 10 names based on the provided instructions ONLY. Do not include a title.";
     let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+
+   
     axios.get(url).then(displaybabynames);
+
 }
+
 
 document.querySelector(".btn-boy").addEventListener("click", function() {
     generatebabynames("boy");
+    
 });
 
 document.querySelector(".btn-girl").addEventListener("click", function() {
-    generatebabynames("girl");
+    generatebabynames("girl"); 
+    
 });
